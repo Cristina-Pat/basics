2c2f6668a557428e4747e018149622fb05f1eff2
Cristina Pat <calota.cristina@gmail.com>
2021-01-29 22:35:33 +0000

    update to even odd counter

--- even_odd_counter.py
+++ even_odd_counter.py
@@ -1,10 +1,32 @@
-list = [12, 14, 2, 3, 9, 11, 10]
+# Reads a list of numbers from a file and computes the number of evn and odd numbers.
+# The first number in the file represents the length of the list.
+
+
+# Open file and read the length of the list
+input_file = open('even_odd_counter.in', 'r')
+nlines = int(input_file.readline())
+print("nlines =", nlines)
+
+# Read the list from the file
+input_list = []
+for i in range(nlines):
+    input_list += [int(input_file.readline())]
+print("The list is", input_list)
+
+
+# Compute the number of even and odd numbers
 even_list = []
 odd_list = []
-for n in list:
+for n in input_list:
     if n%2 == 0:
         even_list += [n]
     else:
         odd_list += [n]
-print(len(even_list))
-print(len(odd_list))
+
+
+# Display the results
+print("Even numbers =", len(even_list))
+print("Odd numbers =", len(odd_list))
+
+# Close the file
+input_file.close()
